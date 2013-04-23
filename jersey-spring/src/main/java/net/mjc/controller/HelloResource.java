1b6636a0cb2d2ddc4b4a4febf3c321de47931817
Matt Cheers <matt.cheers@gmail.com>
2013-04-23 11:38:18 +1000

    added exception handler

--- jersey-spring/src/main/java/net/mjc/controller/HelloResource.java
+++ jersey-spring/src/main/java/net/mjc/controller/HelloResource.java
@@ -4,12 +4,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import java.io.IOException;
 
 @Controller
 public class HelloResource {
@@ -34,4 +37,20 @@ public class HelloResource {
         responseHeaders.set("Hi", name);
         return new ResponseEntity<String>("Hi " + name + ", Spring 3.0!", responseHeaders, HttpStatus.OK);
     }
+
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public void handleException(ResourceNotFoundException ex, HttpServletResponse response) throws IOException
+  {
+    response.sendError(HttpStatus.NOT_FOUND.value(), "Not found for: " + ex.name);
+  }
+
+  private class ResourceNotFoundException extends RuntimeException {
+    private String name;
+
+    public ResourceNotFoundException(String name) {
+      this.name = name;
+    }
+  }
+
 }
